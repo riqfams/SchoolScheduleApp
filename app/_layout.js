1f3dae22be893b1b24f40c7bb717f6eb7e45e515
Ariq Fadhil Musyaffa <ariqfms012@students.unnes.ac.id>
2024-11-18 10:05:38 +0700

    add tailwind

--- app/_layout.js
+++ app/_layout.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { RealmProvider } from '@realm/react'
 import { Stack } from 'expo-router'
+import "../global.css"
 
 export default function AppLayout() {
     return (
         <RealmProvider>
-            <Stack />
+            <Stack screenOptions={{ headerShown: false }} />
         </RealmProvider>
     )
 }
